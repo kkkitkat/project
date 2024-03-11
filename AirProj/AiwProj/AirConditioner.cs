f81794c014752c35a8ac45e4cb7a78f3298582b4
uvev1 <uvev1@FSNIKERS>
2024-03-11 09:12:10 +0300

    add logic temperature

--- AirProj/AiwProj/AirConditioner.cs
+++ AirProj/AiwProj/AirConditioner.cs
@@ -9,5 +9,69 @@ namespace AiwProj
     internal class AirConditioner
     {
         private int temperature;
+
+        private int fanIntensity;
+
+
+        /// <summary>
+        /// Температура
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Неверное значение температуры</exception>
+        public int Temperature
+        {
+            get { return temperature; }
+            set
+            {
+                if (value >= 10 && value <= 30)
+                    temperature = value;
+                else
+                    throw new ArgumentOutOfRangeException("Температура должна быть от 10 до 30 градусов Цельсия.");
+            }
+        }
+
+
+        /// <summary>
+        /// Интенсивность работы
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Неверное значение интенсивности работы</exception>
+        public int FanIntensity
+        {
+            get { return fanIntensity; }
+            set
+            {
+                if (value >= 1 && value <= 10)
+                    fanIntensity = value;
+                else
+                    throw new ArgumentOutOfRangeException("Интенсивность вентилятора должна быть от 1 до 10.");
+            }
+        }
+
+        public AirConditioner(int initialTemperature, int initialFanIntensity)
+        {
+            Temperature = initialTemperature;
+            FanIntensity = initialFanIntensity;
+        }
+
+        /// <summary>
+        /// Повышение температуры на 1 шкалу
+        /// </summary>
+        public void IncreaseTemperature()
+        {
+            if (Temperature < 30)
+                Temperature++;
+        }
+
+        /// <summary>
+        /// Понижение температуры на 1 шкалу
+        /// </summary>
+        public void DecreaseTemperature()
+        {
+            if (Temperature > 10)
+                Temperature--;
+        }
+
+
+
+
     }
 }
